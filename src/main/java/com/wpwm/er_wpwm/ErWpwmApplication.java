1bd775d087f876813738c79c10e4a1cb50808b1f
choozx <90917457+choozx@users.noreply.github.com>
2022-04-11 22:11:18 +0900

    first commit

---
+++ src/main/java/com/wpwm/er_wpwm/ErWpwmApplication.java
@@ -0,0 +1,13 @@
+package com.wpwm.er_wpwm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErWpwmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErWpwmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wpwm/er_wpwm/ErWpwmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wpwm.er_wpwm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErWpwmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
