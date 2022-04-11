f97d949d4e779f4032a8b10e843d671f4d335706
choozx <90917457+choozx@users.noreply.github.com>
2022-04-11 22:30:27 +0900

    홈페이지 초기 세팅

---
+++ src/main/java/com/wpwm/er_wpwm/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.wpwm.er_wpwm.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class TestController {
+
+
+    @GetMapping("/test")
+    public String test() {
+        return "hello world";
+    }
+}
