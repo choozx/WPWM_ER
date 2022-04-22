0c93fd6f45430d966cd0633dfe44276161b4861a
choozx <90917457+choozx@users.noreply.github.com>
2022-04-23 04:05:27 +0900

    GamesRepository 로직 수정

--- src/main/java/com/wpwm/er_wpwm/repository/GamesRepository.java
+++ src/main/java/com/wpwm/er_wpwm/repository/GamesRepository.java
@@ -1,10 +1,7 @@
 package com.wpwm.er_wpwm.repository;
 
 import com.wpwm.er_wpwm.entity.Games;
-import com.wpwm.er_wpwm.repository.mapping.GameIdMapping;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,9 +9,6 @@ import java.util.Optional;
 public interface GamesRepository extends JpaRepository <Games, Long>{
 
     List<Games> findByUserNum(String userNum);
-
-    @Query(value = "select * from games where user_num = ?1 order by game_id desc limit 1)", nativeQuery = true)
-    String findMaxGameIdByUserNum(String user);
 
     Optional<Games> findTopByUserNumOrderByGameIdDesc(String userNum);
 }
